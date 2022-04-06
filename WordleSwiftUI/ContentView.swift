6cf87a0f31fcb9051b9f08c4eb67f8d48a03eec1
cwalter50 <walt50@gmail.com>
2022-04-06 13:27:28 -0400

    Initial Commit

---
+++ WordleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordleSwiftUI
+//
+//  Created by Christopher Walter on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
